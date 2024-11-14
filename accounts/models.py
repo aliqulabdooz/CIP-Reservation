c58fba2e877114b6cd963bc59eb59114fe3ae622
aliqulabdooz <aliqulabdooz@gmail.com>
2024-11-14 16:15:57 +0330

    Cip Porject

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    convoyName = models.CharField(max_length=50, blank=False)  # name caravan
+    phoneNumber = models.CharField(max_length=11, blank=False)
+    profilePicture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELDS = ['email', 'username']
+    REQUIRED_FIELDS = ['convoyName', 'phoneNumber']
+
